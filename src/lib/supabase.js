8cc3949fdc455c79b92599a8051f4f7fd3efdda6
wHallidayy <wtoyakekaveemail@gmail.com>
2026-03-08 15:13:03 +0700

    update env exampl,npm run dev

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xqpqnqswuzsucchweujj.supabase.co';
-const supabaseAnonKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxcHFucXN3dXpzdWNjaHdldWpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NDk0MTUsImV4cCI6MjA4ODUyNTQxNX0.lDMb04rTSGZ3w3xW_Wn0dkzu2-miqnlCTmJIYJcIvc4';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
